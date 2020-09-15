3699a9565720d83e383c55bab4d2b04638fba904
Hailey <10881702@my.uvu.edu>
2020-09-15 17:25:40 -0600

    New Script update

---
+++ New Unity Project 1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 5f;
+    public int score = 100;
+    public float speed = 3f;
+
+    // start is called before the first frame update
+    private void Update()
+    {
+        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(hInput, vInput, 0);
+    }
+
+    // Update is called once per frame
+    public void Up()
+    {
+        print("Up.");
+    }
+
+}
---
+++ New Unity Project 1/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        print("OnTriggerEnter");
+    }
+}
+
+
