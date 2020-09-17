9de82dbfae3daec120c21afde0f6e3edc0e5c11f
Hailey <10881702@my.uvu.edu>
2020-09-16 21:04:11 -0600

    Unity 1.2 tutorials

--- Prototype 1/Assets/Scenes/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scenes/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private object vector3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward 
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward *Time.deltaTime * 20);
     }
 }
