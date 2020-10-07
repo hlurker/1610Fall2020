67d7fe743573b74052ca53185b2cff4b15b66403
Hailey <10881702@my.uvu.edu>
2020-10-06 21:03:04 -0600

    Create with code lesson 4

---
+++ New Unity Project 1/Assets/Scripts/IntData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+        Debug.Log(value);
+    }
+    
+}
