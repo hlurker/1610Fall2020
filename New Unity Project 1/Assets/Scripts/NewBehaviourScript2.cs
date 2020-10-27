d7deec6095480c426936a8c894ef5ce4769c1c56
Hailey <10881702@my.uvu.edu>
2020-10-27 16:00:33 -0600

    List/collections

---
+++ New Unity Project 1/Assets/Scripts/NewBehaviourScript2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections: MonoBehaviour
+{
+    public string singleString;
+    public List<string> collectionList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(singleString);
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project 2/Assets/Scripts/Collections.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections : MonoBehaviour
+{
+    public string singleString;
+    public List<string> collectionList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(singleString);
+        print(collectionList[0]);
+        print(collectionList[1]);
+        print(collectionList[2]);
+        print(collectionList[3]);
+        print(collectionList[4]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
