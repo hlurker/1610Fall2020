397ffd20921aa5b485203eeb929da46c61614b0c
Hailey <10881702@my.uvu.edu>
2020-10-29 19:36:40 -0600

    Scriptable Object
    Thursday Class

---
+++ New Unity Project 2/Assets/Scripts/CollectCash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public IntData cashTotal;
+    public Collectible collectibleObj;
+    public Text txtObj;
+    private SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = collectibleObj.art;
+        renderer.color =
+        txtObj.text = cashTotal.value.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cashTotal.value += 100;
+        txtObj.text = cashTotal.value.ToString();
+        gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
