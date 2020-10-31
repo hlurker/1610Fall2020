42e5dd262289c663f674e85178dc95157aa6a434
Hailey <10881702@my.uvu.edu>
2020-10-31 11:38:29 -0600

    Scriptable Collection data

--- New Unity Project 2/Assets/Scripts/Collections.cs
+++ New Unity Project 2/Assets/Scripts/Collections.cs
@@ -6,9 +6,10 @@ public class Collections : MonoBehaviour
 {
     public string singleString;
     public List<string> collectionList;
+    public List<Collectible> collectibleList;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         print(singleString);
         print(collectionList[0]);
@@ -16,6 +17,18 @@ public class Collections : MonoBehaviour
         print(collectionList[2]);
         print(collectionList[3]);
         print(collectionList[4]);
+
+        for(var i = 0; i <collectibleList.Count; i++)
+        {
+            var position = new Vector3(i * 2, 0, 0);
+            var item = collectibleList[i];
+            var newItem = new GameObject(item.name);
+            newItem.transform.position = position;
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+            sprite.sprite = item.art;
+            sprite.color = item.artColor;
+        }
+    
     }
 
     // Update is called once per frame
