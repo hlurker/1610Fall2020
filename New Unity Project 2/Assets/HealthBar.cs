1cdba61ede173a165e84a94907621eb8032e8682
Hailey <10881702@my.uvu.edu>
2020-11-05 17:13:14 -0700

    Health Bar

---
+++ New Unity Project 2/Assets/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    var Health = 100;
+    var position = Rect(500, 15, 200, 20);
+
+    function OnTriggerEnter(Object : Collider)
+    {
+        if (Object.tag == "Obstacle")
+        {
+            Health -= 50;
+
+        }
+    }
+
+    function Update()
+    {
+        if (Health <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    function OnGUI()
+    {
+        GUI.backgroundColor = Color.green;
+        GUI.HorizontalScrollbar(position, 0, Health, 0, 100);
+    }
