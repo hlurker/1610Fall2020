5adcd8de08038ad662189c5344640626db0ed656
Hailey <10881702@my.uvu.edu>
2020-11-07 15:34:46 -0700

    An Event

---
+++ New Unity Project 2/Assets/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void OnButtonClick();
+    public static event OnButtonClick onButtonClick;
+    public void RaiseOnButtonClick()
+    {
+        if (onButtonClick != null)
+        {
+            onButtonClick();
+        }
+    }
+
+}
