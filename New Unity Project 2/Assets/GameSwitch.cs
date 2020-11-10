353de9534e7d93b4d2da1c300ac169a4f0d1721a
Hailey <10881702@my.uvu.edu>
2020-11-10 15:38:47 -0700

    Enumerations

---
+++ New Unity Project 2/Assets/GameSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameSwitch : ScriptableObject
+{
+    public enum GameStates
+    {
+        Starting,
+        Playing, 
+        Ending,
+        InStore,
+        Pausing
+    }
+
+    public GameStates currentGameState = GameStates.Starting;
+
+    public void ChangeToPlaying()
+    {
+        currentGameState = GameStates.Playing;
+    }
+
+    public void RunCurrentState()
+    {
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                Debug.Log(message: "Starting");
+                break;
+            case GameStates.Playing:
+                Debug.Log(message: "Playing");
+                break;
+            case GameStates.Ending:
+                Debug.Log(message: "Ending");
+                break;
+            case GameStates.InStore:
+                Debug.Log(message: "InStore");
+                break;
+            case GameStates.Pausing:
+                Debug.Log(message: "Pausing");
+                break;
+        }
+    }
+}
