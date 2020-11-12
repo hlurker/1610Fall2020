d4d7c8245e8aa005efdc6b046b2a5c4ad8d3d524
Hailey <10881702@my.uvu.edu>
2020-11-12 15:37:55 -0700

    Script AI

---
+++ New Unity Project 2/Assets/AIBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIBehavior : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = player.position;
+    }
+}
