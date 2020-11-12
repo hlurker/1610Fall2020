1e42dcc7541265e2c22f09c69d77da088fb6a40d
Hailey <10881702@my.uvu.edu>
2020-11-11 18:21:17 -0700

    Weapons to switch

---
+++ New Unity Project 2/Assets/Weapns.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Weapons : ScriptableObject
+{
+    public enum GameStates
+    {
+        Sword,
+        Knife,
+        Gun,
+        Rope,
+    }
+
+    public GameStates currentGameState = GameStates.Sword;
+
+    public void ChangeToPlaying()
+    {
+        currentGameState = GameStates.Sword;
+    }
+
+    public void RunCurrentState()
+    {
+        switch (currentGameState)
+        {
+            case GameStates.Sword:
+                Debug.Log(message: "Sword");
+                break;
+            case GameStates.Knife:
+                Debug.Log(message: "Knife");
+                break;
+            case GameStates.Gun:
+                Debug.Log(message: "Gun");
+                break;
+            case GameStates.Rope:
+                Debug.Log(message: "Rope");
+                break;
+        }
+    }
+}
