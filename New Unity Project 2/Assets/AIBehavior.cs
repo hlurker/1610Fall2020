17ee7890c1d9dce1d90aa73e3d70de3a1246f2e2
Hailey <10881702@my.uvu.edu>
2020-11-17 16:32:39 -0700

    AI Patrolling

--- New Unity Project 2/Assets/AIBehavior.cs
+++ New Unity Project 2/Assets/AIBehavior.cs
@@ -1,20 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-
-public class AIBehavior : MonoBehaviour
+public class AIBehaviour : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform player;
-
+    private Transform destination;
+    private bool canPatrol = true;
+    private int i = 0;
+    public List<Transform> patrolPoints;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        canPatrol = false;
+        destination = other.transform;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canPatrol = true;
     }
 
     private void Update()
     {
-        agent.destination = player.position;
+        agent.destination = destination.position;
+        if (!canPatrol) return;
+        if (agent.pathPending || !(agent.remainingDistance < 0.5f)) return;
+        destination = patrolPoints[i];
+        i = (i + 1) % patrolPoints.Count;
     }
 }
