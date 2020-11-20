b3eedf44c2e24935b4f2a8bb62451335bf3a134c
Hailey <10881702@my.uvu.edu>
2020-11-19 17:52:59 -0700

    Coroutines with Loops

---
+++ New Unity Project 2/Assets/CoroutineEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class CoroutineEvents : MonoBehaviour
+{
+    public UnityEvent StartEvent, repeatEvent, endEvent;
+    public float holdtime, repeatHoldTime = 0.25f;
+    public int counter = 3;
+
+    private IEnumerator Corountine()
+    {
+        StartEvent.Invoke();
+
+        yield return new WaitForSeconds(holdTime);
+
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+            counter--;
+        }
+
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}
