b5d2a50c03bc68ce79644dc3df11e67eca18189e
Hailey <10881702@my.uvu.edu>
2020-12-01 16:12:25 -0700

    Animation Controllers

---
+++ New Unity Project 2/Assets/Scripts/AttachOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AttachOnTrigger : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ground"))
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print(other);
+        transform.parent = null; 
+    }
+}
